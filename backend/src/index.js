2b9d2263830b3a7f70e514f3af37cb0b0c66bc0c
Michael Umoize <michaelumoize@gmail.com>
2026-06-12 20:47:20 +0300

    initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+// const express = require('express');
+import express from 'express';
+import "dotenv/config";
+
+const app = express();
+
+const PORT = process.env.PORT
+
+
+
+app.listen(PORT, () => {
+  console.log('Server is running on PORT:', PORT);
+});
